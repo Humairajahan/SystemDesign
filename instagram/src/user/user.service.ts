2136d475a65da282eed2c7ed7a98985d814b4654
Humairajahan <humairaazim45@gmail.com>
2024-10-27 16:25:27 +0600

    `UserService`: `create` route created

--- instagram/src/user/user.service.ts
+++ instagram/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -12,8 +12,53 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private userOlderThan13(dob: Date) {
+    const today = new Date();
+    let currentAge = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+      currentAge--;
+    }
+    return currentAge >= 13;
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // Ensure no duplication of email or instaHandle
+    const userExists = await this.userRepository.findOne({
+      where: [
+        { email: createUserDto.email },
+        { instaHandle: createUserDto.instaHandle },
+      ],
+    });
+
+    if (userExists) {
+      const duplicateField =
+        userExists.email === createUserDto.email ? 'email' : 'handle';
+      throw new HttpException(
+        `User with this ${duplicateField} exists`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    // Ensure user is of at least 13 years old
+    const dob = new Date(createUserDto.dateOfBirth);
+    if (!this.userOlderThan13(dob)) {
+      throw new HttpException(
+        'User must be older than 13',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const user = this.userRepository.create({
+      name: createUserDto.name,
+      email: createUserDto.email,
+      instaHandle: createUserDto.instaHandle,
+      DOB: createUserDto.dateOfBirth,
+      password: createUserDto.password,
+      avatarUrl: createUserDto.avatarUrl,
+    });
+
+    return await this.userRepository.save(user);
   }
 
   findAll() {
