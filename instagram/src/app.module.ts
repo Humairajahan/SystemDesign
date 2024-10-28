3bfecadd932e1f6fd5d9263e535c99f6331520a8
Humairajahan <humairaazim45@gmail.com>
2024-10-28 11:37:15 +0600

    Relocate `user` module to `modules` directory

--- instagram/src/app.module.ts
+++ instagram/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from './config/typeorm.config';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
