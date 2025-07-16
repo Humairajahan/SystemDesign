56dea5d4f2bcbeed7c98661179d195aeb56d2467
humairaJahan <humairaazim45@gmail.com>
2025-07-16 16:19:06 +0600

    Restructuring repository. Delete instagram directory

--- instagram/src/config/swagger.config.ts
+++
@@ -1,7 +0,0 @@
-import { DocumentBuilder } from '@nestjs/swagger';
-
-export const swaggerConfig = new DocumentBuilder()
-  .setTitle('Instagram')
-  .setDescription('The Collection of APIs for Instagram')
-  .setVersion('1.0')
-  .build();
--- instagram/src/modules/user/dto/update-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { RegistrationDto } from '../../auth/dto/registration.dto';
-
-export class UpdateUserDto extends PartialType(RegistrationDto) {}
